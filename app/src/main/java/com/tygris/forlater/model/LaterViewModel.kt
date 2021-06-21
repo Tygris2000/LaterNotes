fd1ec11552fc7611254d8b03da8bb59cf8197b75
Tygris <youssif6920@gmail.com>
2021-06-21 10:38:33 +0300

    first commit

--- app/src/main/java/com/tygris/forlater/model/LaterViewModel.kt
+++ app/src/main/java/com/tygris/forlater/model/LaterViewModel.kt
@@ -1,20 +1,13 @@
 package com.tygris.forlater.model
 
 import android.app.Application
-import android.content.SharedPreferences
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkRequest
 import com.tygris.forlater.data.Reposit
 import com.tygris.forlater.data.UserPrefs
-import com.tygris.forlater.worker.NotifyWorker
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.*
-import java.util.concurrent.TimeUnit
 
 class LaterViewModel(application: Application) : AndroidViewModel(application) {
     val repoi : Reposit = Reposit.getInstance()
@@ -25,11 +18,6 @@ class LaterViewModel(application: Application) : AndroidViewModel(application) {
 
 
     fun getAll() : LiveData<List<SomethingForLater>> = repoi.getAll()
-    fun workerRegister(date : Date){
-      val workreq :WorkRequest = OneTimeWorkRequestBuilder<NotifyWorker>()
-          .setInitialDelay(2,TimeUnit.MINUTES).build()
-
-    }
     fun insertNew(something : SomethingForLater){
         viewModelScope.launch(Dispatchers.IO){
             repoi.insert(something)
